71de93d1a2b882e03762d047f1efa09a4f5389a5
Somnath Ghosh <sg1941627@gmail.com>
2025-12-16 14:27:38 +0530

    Update server URL in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createContext } from "react";
 export const authDataContext = createContext();
 function AuthContext({ children }) {
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = "https://ai-powered-e-commerce-website-backend-e7kk.onrender.com";
 
   let value = {
     serverUrl,
